1c2bebc610d12054a818d94b9bd20d45d9f2ede4
Eleanor Jackson <eleanor.elizabeth.j@gmail.com>
2024-06-01 12:19:36 +0100

    "control" rather than "untreated" for fig labs

--- code/scripts/make-figures-methods.R
+++ code/scripts/make-figures-methods.R
@@ -50,7 +50,7 @@ all_runs %>%
   sample_n(1) %>%
   unnest(df_train) %>%
   left_join(nfi_coords, by = "description") %>%
-  mutate(tr = recode_factor(tr, `0` = "Untreated", `1` = "Treated")) %>%
+  mutate(tr = recode_factor(tr, `0` = "Control", `1` = "Treated")) %>%
   ggplot(aes(ost_wgs84, nord_wgs84, colour = tr)) +
   borders("world", regions = "sweden", linewidth = 0.25) +
   geom_point(size = 0.25, alpha = 0.7, shape = 16) +
@@ -62,7 +62,7 @@ all_runs %>%
         plot.margin = unit(c(0, 0, 0, 0), "cm"),
         legend.key.size = unit(0.01, "cm")) +
   guides(color = guide_legend(override.aes = list(size = 2))) +
-  scale_colour_manual(values = c("Treated" = "#E69F00", "Untreated" = "#0072B2")) +
+  scale_colour_manual(values = c("Treated" = "#E69F00", "Control" = "#0072B2")) +
   facet_wrap(~ assignment, nrow = 1)
 
 
@@ -79,7 +79,7 @@ all_runs %>%
   sample_n(1) %>%
   unnest(df_train) %>%
   left_join(nfi_coords, by = "description") %>%
-  mutate(tr = recode_factor(tr, `0` = "Untreated", `1` = "Treated")) %>%
+  mutate(tr = recode_factor(tr, `0` = "Control", `1` = "Treated")) %>%
   ggplot(aes(ost_wgs84, nord_wgs84, colour = tr)) +
   borders("world", regions = "sweden", linewidth = 0.25) +
   geom_point(size = 0.25, alpha = 0.9, shape = 16) +
@@ -90,7 +90,7 @@ all_runs %>%
         legend.text = element_text(size = 8),
         plot.margin = unit(c(0, 0, 0, 0), "cm")) +
   guides(color = guide_legend(override.aes = list(size = 2))) +
-  scale_colour_manual(values = c("Treated" = "#E69F00", "Untreated" = "#0072B2")) +
+  scale_colour_manual(values = c("Treated" = "#E69F00", "Control" = "#0072B2")) +
   facet_wrap(~ prop_not_treated, nrow = 1)
 
 ggsave(here::here("output","figures","methods-prop_not_treated.png"),
@@ -107,7 +107,7 @@ all_runs %>%
   sample_n(1) %>%
   unnest(df_train) %>%
   left_join(nfi_coords, by = "description") %>%
-  mutate(tr = recode_factor(tr, `0` = "Untreated", `1` = "Treated")) %>%
+  mutate(tr = recode_factor(tr, `0` = "Control", `1` = "Treated")) %>%
   ggplot(aes(ost_wgs84, nord_wgs84, colour = tr)) +
   borders("world", regions = "sweden", linewidth = 0.25) +
   geom_point(size = 0.25, alpha = 0.7, shape = 16) +
@@ -118,7 +118,7 @@ all_runs %>%
         legend.text = element_text(size = 8),
         plot.margin = unit(c(0, 0, 0, 0), "cm")) +
   guides(color = guide_legend(override.aes = list(size = 2))) +
-  scale_colour_manual(values = c("Treated" = "#E69F00", "Untreated" = "#0072B2")) +
+  scale_colour_manual(values = c("Treated" = "#E69F00", "Control" = "#0072B2")) +
   facet_wrap(~ n_train, nrow = 1)
 
 ggsave(here::here("output","figures","methods-n_train.png"),
@@ -142,7 +142,7 @@ all_runs %>%
     .ordered = TRUE
   )) %>%
   left_join(nfi_coords, by = "description") %>%
-  mutate(tr = recode_factor(tr, `0` = "Untreated", `1` = "Treated")) %>%
+  mutate(tr = recode_factor(tr, `0` = "Control", `1` = "Treated")) %>%
   ggplot(aes(ost_wgs84, nord_wgs84, colour = tr)) +
   borders("world", regions = "sweden", linewidth = 0.25) +
   geom_point(size = 0.25, alpha = 0.7, shape = 16) +
@@ -153,7 +153,7 @@ all_runs %>%
         legend.text = element_text(size = 8),
         plot.margin = unit(c(0, 0, 0, 0), "cm")) +
   guides(color = guide_legend(override.aes = list(size = 2))) +
-  scale_colour_manual(values = c("Treated" = "#E69F00", "Untreated" = "#0072B2")) +
+  scale_colour_manual(values = c("Treated" = "#E69F00", "Control" = "#0072B2")) +
   facet_wrap(~ sampling_location, nrow = 1)
 
 ggsave(here::here("output","figures","methods-test_plot_location.png"),
